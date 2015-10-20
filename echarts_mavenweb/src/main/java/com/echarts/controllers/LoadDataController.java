b88f578267d5657ac356c803cb378f6e82a727c3
wangzhaod <wangzhaod@yonyou.com>
2015-10-20 14:21:15 +0800

    inject service to controller
    Change-Id: Ib440170aafcdb4cf9ebfcb5ecd65fba51c80c455
    Signed-off-by: wangzhaod <wangzhaod@yonyou.com>

--- echarts_mavenweb/src/main/java/com/echarts/controllers/LoadDataController.java
+++ echarts_mavenweb/src/main/java/com/echarts/controllers/LoadDataController.java
@@ -1,26 +1,23 @@
 package com.echarts.controllers;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.echarts.models.PieDataModel;
 import com.echarts.models.User;
+import com.echarts.services.LoadDataService;
 
 @Controller
 public class LoadDataController {
-
+	@Autowired
+	//@Qualifier("loaddataservice")
+	private LoadDataService service;
+	
 	@RequestMapping(value = "/signin.jsonp")
-
 	public User signIn(@RequestParam("name") String userName) {
 		System.out.println(userName);
 		
@@ -54,21 +51,7 @@ public class LoadDataController {
 	@RequestMapping(value = "/loaddata.jsonp"/*, method = { RequestMethod.POST }
 					headers = { "Accept=application/json" }*/)
 	//@ResponseBody
-	public PieDataModel[] loadData() {
-		//System.out.println(user);
-		PieDataModel data1 = new PieDataModel();
-		data1.setName("A");
-		data1.setValue(335);
-		PieDataModel data2 = new PieDataModel();
-		data2.setName("B");
-		data2.setValue(679);
-		PieDataModel data3 = new PieDataModel();
-		data3.setName("C");
-		data3.setValue(1548);
-		
-		PieDataModel[] datas = new PieDataModel[] { data1, data2, data3 };		
-		
-		return datas;
-		//"[{\"value\":335,\"name\":\"A\"},{\"value\":679,\"name\":\"B\"},{\"value\":1548,\"name\":\"C\"}]"; 
+	public PieDataModel[] loadData() {				
+		return service.loadData();		 
 	}
 }
